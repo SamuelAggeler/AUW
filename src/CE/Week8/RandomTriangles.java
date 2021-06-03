2c41d9a14f70e10e4a2d126de4e989c36be25317
Samuel Aggeler <Samuel.aggeler@outlook.com>
2021-06-03 17:40:35 +0200

    Updated RandomTriangles

--- src/CE/Week8/RandomTriangles.java
+++ src/CE/Week8/RandomTriangles.java
@@ -3,29 +3,36 @@ import java.util.*;
 
 
 public class RandomTriangles {
-    int triangles;
-    double prob;
-
-
-
-    
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        RandomTriangles test = new RandomTriangles();
-        test.triangles = scanner.nextInt();
-        test.prob = scanner.nextDouble();
-        scanner.close();
-        System.out.println(test.prob() + "" + test.var());
+        // Uncomment this line if you want to read from a file
+        In.open("public/test1.in");
+        Out.compareTo("public/test1.out");
 
+        int t = In.readInt();
+        for (int i = 0; i < t; i++) {
+            testCase();
+        }
+        
+        // Uncomment this line if you want to read from a file
+        // In.close();
     }
 
+    public static void testCase() {
+        // Input using In.java class
+        int n = In.readInt();
+        double p = In.readDouble();
+        double binom = (double) Algorithms.binom(n, n-3);
+        double ex = (double) binom*p*p*p;
+        double varianz = 0;
 
-    public double prob() {
-        return 0.0;
-    }
-
-    public double var() {
-        return 0.0;
+          double firstcase = binom*p*p*p;
+          double secondcase = binom*3*(n-3)*p*p*p*p*p;
+          double thirdcase = (binom*binom-binom-binom*3*(n-3))*p*p*p*p*p*p;
+          varianz = firstcase + secondcase + thirdcase - (ex*ex);
+        
+        
+        // Output using Out.java class
+        Out.println(ex + " " + varianz);                                                                                                              
     }
 
 }
